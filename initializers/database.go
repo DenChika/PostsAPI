b8c469b99c7c60819d1f9b73dc382486859655c6
DenChika <eholocation@gmail.com>
2023-02-10 03:23:55 +0300

    refactor: move db string to env

--- initializers/database.go
+++ initializers/database.go
@@ -4,13 +4,14 @@ import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
+	"os"
 )
 
 var DB *gorm.DB
 
 func ConnectToDB() {
 	var err error
-	dsn := "host=salt.db.elephantsql.com user=zgenpbbc password=EuRwgrEsl2UggpSP__Xg8e4wmvvhIzvC dbname=zgenpbbc port=5432 sslmode=disable"
+	dsn := os.Getenv("DB_URL")
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database!")
